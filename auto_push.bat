4081232f151fae1b358a9dfccfe2e2e13c2b90f2
rebinocity (cosmoo-the-cow) <ghazijaber3@outlook.com>
2026-06-13 23:33:23 +0400

    auto update

--- auto_push.bat
+++ auto_push.bat
@@ -2,25 +2,26 @@
 cd /d C:\Users\user\Desktop\github\calculus-speedrun
 
 echo ==========================
-echo   git auto push
+echo   git auto push tool
 echo ==========================
 
+echo pulling latest changes...
+git pull origin main --rebase
+
+echo.
 echo adding files...
 git add .
 
 echo.
-
 echo committing changes...
 git commit -m "auto update"
 
 echo.
-
-echo pushing to GitHub...
+echo pushing to github...
 git push origin main
 
 echo.
 echo opening website...
-
 start https://cosmoo-the-cow.github.io/calculus-speedrun/
 
 echo.
